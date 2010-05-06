3a45fc900b76dc9f39b704c4be7472fe96d6e2db
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-05-06 11:55:48 +0000

    now i have the 2 options for the double field

--- uvc.widgets/trunk/uvc/widgets/jslibrary/double.js
+++ uvc.widgets/trunk/uvc/widgets/jslibrary/double.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $.fn.extend({
+/*
+ $.fn.extend({
   appendField: function(id) {
 
       $(this).wrap('<table class="field-liner"><tr><td class="lined-field"></td></tr></table>');
@@ -9,4 +10,30 @@ $(document).ready(function() {
       }
    });
    $('#form-widgets-name-row').appendField('#form-widgets-vorname-row');
+*/
+
+    $.fn.extend({
+  appendFieldTo: function(id) {
+          input = $(this).find('div.widget input');
+          label = $(this).find('label span.label').html();
+          description = $(this).find('label span.small').html();
+          error = $(this).find('div.error').html();
+          input.appendTo(id + ' div.widget');
+          $(id + ' label span.label').append(', ' + label);
+          $(id + ' label span.small').append(', ' + description);
+          if ($(this).attr('class') == 'row row_error'){
+             $(id).addClass('row_error');
+             if ($(id).is('.error')) {
+               $(id + ' error').append(error);
+             } 
+             else {
+               $(id).append('<div class="error"> ' + error + ' </div>')
+             }
+          }
+          $(this).hide();
+        }
+     })
+   $('#form-widgets-vorname-row').appendFieldTo('#form-widgets-name-row');
+
+
 });
