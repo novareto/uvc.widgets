71e5ec5230bfc07dc7e780d6f7840e5381ab87f8
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-05-06 09:52:28 +0000

    fixed styles.

--- uvc/widgets/jslibrary/double.js
+++ uvc/widgets/jslibrary/double.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       $(this).wrap('<table class="field-liner"><tr><td class="lined-field"></td></tr></table>');
 
       $(this).closest('.lined-field').after($(id));
-      $(id).wrap('<td></d>');
+      $(id).wrap('<td class="lined-field"></d>');
       }
    });
    $('#form-widgets-name-row').appendField('#form-widgets-vorname-row');
