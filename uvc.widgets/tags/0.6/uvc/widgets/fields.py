a16c9c7d6fe30576881e0fa6df3d5d7c5c7bf85c
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-06-01 11:42:33 +0000

    Tagging 0.6

---
+++ uvc.widgets/tags/0.6/uvc/widgets/fields.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from zope.schema import Choice
+from zope.interface import Interface, directlyProvides
+
+
+class IAlternativeChoice(Interface):
+    pass
+
+
+class OptionalChoice(Choice):
+    """A choice field with the option to add an alternative value
+    """
+    def __init__(self, values=None, vocabulary=None,
+                 source=None, alternative=False, **kw):
+        Choice.__init__(self, values, vocabulary, source, **kw)
+        self.alternative = alternative
+        if alternative is True:
+            directlyProvides(self, IAlternativeChoice)
+
+    def _validate(self, value):
+        if self._init_field:
+            return
+        
+        if not self.alternative:
+            vocabulary = self.vocabulary
+            if vocabulary is None:
+                vr = getVocabularyRegistry()
+                try:
+                    vocabulary = vr.get(None, self.vocabularyName)
+                except VocabularyRegistryError:
+                    raise ValueError("Can't validate value without vocabulary")
+                if value not in vocabulary:
+                    raise ConstraintNotSatisfied(value)
