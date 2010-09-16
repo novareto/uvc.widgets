dc0fb814182907d5ca2e514aa4f4ea887545572b
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-09-16 12:00:27 +0000

    changes

--- uvc.widgets/trunk/uvc/widgets/jslibrary/datepicker.js
+++ uvc.widgets/trunk/uvc/widgets/jslibrary/datepicker.js
@@ -1,6 +1,6 @@
 $(function() {
   $.each($('input.field-date'), function() {  
-     $(this).dateinput({ format: 'dd.mm.yyyy', firstDay: 1  });
+     $(this).dateinput({ format: 'dd.mm.yyyy', 'firstDay': 1 });
   })
 });
 
