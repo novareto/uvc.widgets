643f63b5470403106b1debabd453947feb84f54a
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-02-15 08:41:44 +0000

    german datepicker

--- uvc.widgets/trunk/uvc/widgets/jslibrary/datepicker.js
+++ uvc.widgets/trunk/uvc/widgets/jslibrary/datepicker.js
@@ -1,3 +1,12 @@
+$.tools.dateinput.localize("de", {
+   months: 'Januar,Februar,März,April,Mai,Juni,Juli,August,September,Oktober,November,Dezember',
+   shortMonths:  'Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Okt,Nov,Dez',
+   days:         'Sonntag,Montag,Dienstag,Mittwoch,Donnerstag,Freitag,Samstag',
+   shortDays:    'Son,Mon,Die,Mit,Do,Fri,Sam'
+});
+
+$.tools.dateinput.conf.lang = 'de';
+
 $(function() {
   $.each($('input.field-date'), function() {  
      $(this).dateinput({ format: 'dd.mm.yyyy', 'firstDay': 1 });
