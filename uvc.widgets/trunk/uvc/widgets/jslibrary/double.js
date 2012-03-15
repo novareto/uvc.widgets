8a5f06f19ac04ed02eadb0052375e598ab94584e
ck@novareto.de <ck@novareto.de@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2012-03-15 13:57:44 +0000

    changes

--- uvc.widgets/trunk/uvc/widgets/jslibrary/double.js
+++ uvc.widgets/trunk/uvc/widgets/jslibrary/double.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
     $.fn.extend({
   appendFieldTo: function(id) {
           input = $(this).find('input');
-          label = $(this).find('label control-label').html();
+          label = $(this).find('label.control-label').html();
           description = $(this).find('p.help-block').html();
           error = $(this).find('div.error').html();
+          $(id + ' input').css('margin-right', '1em');
           input.insertAfter(id + ' input');
           $(id + ' label.control-label').append(', ' + label);
           $(id + ' p.help-block').append(', ' + description);
-          if ($(this).attr('class') == 'row row_error'){
-             $(id).addClass('row_error');
+          if ($(this).attr('class') == 'control-group error'){
+             $(id).addClass('error');
              if ($(id).is('.error')) {
                $(id + ' error').append(error);
              } 
