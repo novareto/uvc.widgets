00cbb4cd54275313b6fad7224074e0a62b9eb9b5
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2012-03-22 14:47:16 +0000

    chagnes

--- uvc/widgets/resources.py
+++ uvc/widgets/resources.py
@@ -8,15 +8,10 @@ from js.jquery_tools import jquery_tools
 from fanstatic import Resource, Library
 
 
-widget_library = Library('uvc.widgets', 'jslibrary')
+widget_library = Library('uvc.widgets', 'static')
 
 
 DatePickerCSS = Resource(widget_library, 'datepicker.css')
-
 DatePicker = Resource(widget_library, 'datepicker.js', depends=[jquery_tools])
-
 optchoice = Resource(widget_library, 'choice.js', depends=[jquery])
-
 double = Resource(widget_library, 'double.js', depends=[jquery])
-
-validation = Resource(widget_library, 'validation.js', depends=[jquery])
