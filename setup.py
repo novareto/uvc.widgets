6f21cce3432f27e94be2483b63520ec8c5220afe
ck <ck@novareto.de>
2013-02-01 08:46:41 +0000

    Preparing release 0.9.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.9.6.dev0'
+version = '0.9.6'
 
 setup(name='uvc.widgets',
       version=version,
@@ -24,6 +24,7 @@ setup(name='uvc.widgets',
           'setuptools',
           'js.jquery_tools',
           'js.jquery',
+          'js.jquery_maskmoney'
           # -*- Extra requirements: -*-
       ],
       entry_points={
