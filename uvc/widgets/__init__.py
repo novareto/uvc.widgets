a0e3acbc30b6df0d7fa36de7f627f01c05dca386
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2014-07-14 23:01:55 +0000

    Started the uvclight migration

--- uvc/widgets/__init__.py
+++ uvc/widgets/__init__.py
@@ -1,4 +1,3 @@
 from resources import double, DatePickerCSS, DatePicker, masked_input, lov, plz_select
 from uvc.widgets.fields import OptionalChoice
-from uvc.widgets.components import AjaxPLZOrt
-
+#from uvc.widgets.components import AjaxPLZOrt
