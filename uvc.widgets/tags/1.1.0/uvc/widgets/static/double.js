53195d3922022ff54739b43b9cf8e226d36b59c2
ck@novareto.de <ck@novareto.de@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2015-07-28 09:34:26 +0000

    Tagging 1.1.0

---
+++ uvc.widgets/tags/1.1.0/uvc/widgets/static/double.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $.fn.extend({
+  appendFieldTo: function(id) {
+          input = $(this).find('input');
+          label = $(this).find('label.control-label').html();
+          description = $(this).find('p.help-block').html();
+          error = '&nbsp;' + $(this).find('span#error-message').html();
+          $(id + ' input').css('margin-right', '1em');
+          
+          $(id + ' input').wrap(jQuery('<div class="form-inline"></div>'));
+          input.insertAfter(id + ' input');
+          
+          $(id + ' label.control-label').append(', ' + label);
+          $(id + ' p.help-block').append(', ' + description);
+          if ($(this).hasClass('has-error')){
+             if ($(id).hasClass('has-error')) {
+               $(id + ' span#error-message').append(error);
+             }
+             else {
+               $(id).addClass('form-group alert-danger has-error');
+               $(id + ' :input:last').after('<p class="help-block"> ' + error + ' </span>')
+             }
+          }
+          $(this).hide();
+        }
+     })
+});
+
